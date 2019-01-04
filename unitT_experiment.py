084d90b5e273a511809dc0b54d3df35b54bdb986
Xu Chen <xu.chen@dal.ca>
2019-01-04 20:44:58 +0800

    Update: add unit test for gen_adv

--- unitT_experiment.py
+++ unitT_experiment.py
@@ -19,7 +19,7 @@ import shutil
 import numpy as np 
 import tensorflow as tf 
 
-from experiment import default_hparams, train, test
+from experiment import default_hparams, train, test, gen_adv
 
 hparams = default_hparams()
 
@@ -54,6 +54,32 @@ class ExperimentTest(tf.test.TestCase):
              total_batch_size=200, image_size=28,
              summary_dir='debug/summary/caps/svhn/Default/',
              load_test_path='debug/data/caps/svhn/test.npz')
+
+    """Generate adversarial exmaples"""
+    def testGenAdvMNIST(self):
+        gen_adv(num_gpus=2, data_dir='debug/data/caps/mnist/', dataset='mnist',
+                adversarial_method='FGSM',
+                total_batch_size=2, image_size=28,
+                summary_dir='debug/summary/caps/mnist/Default/', all_=2)
+
+        with np.load(os.path.join('debug/data/caps/mnist/', 'test_FGSM.npz')) as f:
+            images, labels = f['x'], f['y']
+        
+        self.assertEqual((4, 28, 28, 1), images.shape)
+        self.assertEqual((4,), labels.shape)
+
+    def testGenAdvSVHN(self):
+        gen_adv(num_gpus=2, data_dir='debug/data/caps/svhn',
+        dataset='svhn',
+                adversarial_method='FGSM',
+                total_batch_size=2, image_size=28, 
+                summary_dir='debug/summary/caps/svhn/Default/', all_=2)
+
+        with np.load(os.path.join('debug/data/caps/svhn/', 'test_FGSM.npz')) as f:
+            images, labels = f['x'], f['y']
+        
+        self.assertEqual((4, 28, 28, 3), images.shape)
+        self.assertEqual((4,), labels.shape)
 
 if __name__ == '__main__':
     tf.test.main()
