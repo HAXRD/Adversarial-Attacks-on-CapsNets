5cdd67015a129636ff61f0060eda796412b6a08a
Xu Chen <xu.chen@dal.ca>
2019-01-28 06:39:43 -0400

    Update: scripts for gen_adv

---
+++ scripts_f/caps_r/fashion_mnist/gen_adv/gen_BIM_ep1_iter2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --gres=gpu:2        # request GPU "generic resource"
+#SBATCH --cpus-per-task=6   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
+#SBATCH --mem=15000M        # memory per node
+#SBATCH --time=0-02:00      # time (DD-HH:MM)
+#SBATCH --output=scripts_f/caps_r/fashion_mnist/gen_adv/o_gen_BIM_ep1_iter2.out  # %N for node name, %j for jobID
+
+source ~/tfp363/bin/activate
+
+REPO_DIR=/home/xuc/Adversarial-Attack-on-CapsNets
+SUMMARY_DIR=/home/xuc/projects/def-sageev/xuc/summary/
+
+MODEL=caps_r
+DATASET=fashion_mnist
+
+ADVERSARIAL_METHOD=BIM 
+EPSILON=1
+ITERATION_N=2
+
+python $REPO_DIR/experiment.py --mode=gen_adv --data_dir=$REPO_DIR/data/$MODEL/$DATASET --dataset=$DATASET --adversarial_method=$ADVERSARIAL_METHOD --epsilon=$EPSILON --iteration_n=$ITERATION_N --summary_dir=$SUMMARY_DIR/$MODEL/$DATASET/Default/
