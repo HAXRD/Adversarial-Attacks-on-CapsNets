0b1ec349a22fe1160eeed5c6f2a439866628a81d
Xu Chen <xu.chen@dal.ca>
2019-01-27 20:04:05 -0400

    Update: new scripts for training

---
+++ scripts_f/caps/cifar10/train/train.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --gres=gpu:2        # request GPU "generic resource"
+#SBATCH --cpus-per-task=6   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
+#SBATCH --mem=15000M        # memory per node
+#SBATCH --time=0-08:00      # time (DD-HH:MM)
+#SBATCH --output=scripts_f/caps/cifar10/train/o_train.out  # %N for node name, %j for jobID
+
+
+source ~/tfp363/bin/activate
+
+REPO_DIR=/home/xuc/Adversarial-Attack-on-CapsNets
+SUMMARY_DIR=/home/xuc/projects/def-sageev/xuc/summary/
+
+MODEL=caps
+DATASET=cifar10
+
+python $REPO_DIR/experiment.py --data_dir=$REPO_DIR/data/$MODEL/$DATASET --dataset=$DATASET --summary_dir=$SUMMARY_DIR --model=$MODEL --hparams_override=remake=false
---
+++ scripts_f/caps/mnist/train/train_BIM_ep4_iter1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --gres=gpu:2        # request GPU "generic resource"
+#SBATCH --cpus-per-task=6   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
+#SBATCH --mem=15000M        # memory per node
+#SBATCH --time=0-08:00      # time (DD-HH:MM)
+#SBATCH --output=scripts_f/caps/mnist/train/o_train_BIM_ep4_iter1.out  # %N for node name, %j for jobID
+
+
+source ~/tfp363/bin/activate
+
+REPO_DIR=/home/xuc/Adversarial-Attack-on-CapsNets
+SUMMARY_DIR=/home/xuc/projects/def-sageev/xuc/summary/
+
+MODEL=caps
+DATASET=mnist
+
+ADVERSARIAL_METHOD=BIM 
+EPSILON=4
+ITERATION_N=1
+
+python $REPO_DIR/experiment.py --data_dir=$REPO_DIR/data/$MODEL/$DATASET --dataset=$DATASET --adversarial_method=$ADVERSARIAL_METHOD --epsilon=$EPSILON --iteration_n=$ITERATION_N --summary_dir=$SUMMARY_DIR --model=$MODEL --hparams_override=remake=false
