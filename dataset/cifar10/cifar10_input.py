3079922455e575b1cce69fe10740fd91a45aa298
Xu Chen <xu.chen@dal.ca>
2019-01-28 19:01:40 -0400

    Update: cifar10 input and scripts

--- dataset/cifar10/cifar10_input.py
+++ dataset/cifar10/cifar10_input.py
@@ -60,10 +60,18 @@ def _single_process(image, label, specs, resized_size):
         if specs['split'] == 'train':
             # random cropping
             image = tf.random_crop(image, [resized_size, resized_size, 3])
+            # random flip
+            image = tf.image.random_flip_left_right(image)
+            # random brightness
+            image = tf.image.random_brightness(image, max_delta=63)
+            # random contrast
+            image = tf.image.random_contrast(image, lower=0.2, upper=1.8)
+            image = tf.image.per_image_standardization(image)
         elif specs['split'] == 'test':
             # central cropping
             image = tf.image.resize_image_with_crop_or_pad(
                 image, resized_size, resized_size)
+            image = tf.image.per_image_standardization(image)
     # convert into 0. ~ 1. 
     image = tf.cast(image, tf.float32)
     
