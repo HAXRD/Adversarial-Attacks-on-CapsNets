2e7654f306ead39758db8b31f2bf16e2fe713ae1
Xu Chen <xu.chen@dal.ca>
2019-02-01 19:53:23 -0400

    Update: train scripts

--- scripts/cnn/cifar10/train/train_BIM_ep1_iter8.sh
+++ scripts/cnn/cifar10/train/train_BIM_ep1_iter8.sh
@@ -2,7 +2,7 @@
 #SBATCH --gres=gpu:2        # request GPU "generic resource"
 #SBATCH --cpus-per-task=6   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=15000M        # memory per node
-#SBATCH --time=0-16:00      # time (DD-HH:MM)
+#SBATCH --time=0-04:00      # time (DD-HH:MM)
 #SBATCH --output=scripts/cnn/cifar10/train/o_train_BIM_ep1_iter8.out  # %N for node name, %j for jobID
 
 
