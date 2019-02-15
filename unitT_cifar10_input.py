71faa7944b4da5cd2e77ddb278b639c89b7adf9f
Xu Chen <xu.chen@dal.ca>
2019-02-14 23:22:30 -0400

    Updating test files

--- unitT_cifar10_input.py
+++ unitT_cifar10_input.py
@@ -1,17 +1,3 @@
-# Copyright 2018 Xu Chen All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
 """Tests for cifar10_input"""
 
 import os
@@ -42,7 +28,7 @@ class Cifar10InputTest(tf.test.TestCase):
                 num_gpus=2,
                 max_epochs=1,
                 resized_size=28,
-                data_dir=os.path.join(out_dir, 'caps', 'svhn'),
+                data_dir=os.path.join(out_dir, 'caps', 'cifar10'),
                 split='train')
             iterator = batched_dataset.make_initializable_iterator()
             sess.run(iterator.initializer)
--- unitT_mnist_input.py
+++ unitT_mnist_input.py
@@ -1,17 +1,3 @@
-# Copyright 2018 Xu Chen All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
 """Tests for mnist_input"""
 
 import os
@@ -50,6 +36,8 @@ class MnistInputTest(tf.test.TestCase):
             batch_val = sess.run(batch_data)
             images, labels = batch_val['images'], batch_val['labels']
 
+            self.assertEqual(tf.float32, images.dtype)
+            self.assertEqual(tf.float32, labels.dtype)
             self.assertEqual((100, 10), labels.shape)
             self.assertItemsEqual([0, 1], np.unique(labels))
             self.assertEqual(28, specs['image_size'])
